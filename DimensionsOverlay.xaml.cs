4f80e0642fa259f3a9a98de0192e85d7378e3a34
Benry Yip <benry.yip@ni.com>
2013-01-28 17:04:17 -0600

    Added calls to screenshot utility. DOES NOT WORK: fix calls on wrong thread.

--- DimensionsOverlay.xaml.cs
+++ DimensionsOverlay.xaml.cs
@@ -6,33 +6,71 @@ using MahApps.Metro.Controls;
 
 namespace wpfScope
 {
-    /// <summary>
-    /// Interaction logic for DimensionsOverlay.xaml
-    /// </summary>
     public partial class DimensionsOverlay : MetroWindow
     {
+        #region Private Fields
+
         private object _lock = new object(); // NO HEISENBUGS.
         private bool _shouldUpdateAnalysis;
         private BackgroundWorker _updateAnalysisWorker;
 
+        #endregion
+
+        #region Properties
+
+        private DimensionsAnalyzer Analyzer { get; set; }
+
+        #endregion
+
+        #region Constructors
+
         public DimensionsOverlay()
         {
             InitializeComponent();
+
+            Analyzer = new DimensionsAnalyzer();
+
+            DisableAnalysisUpdate();
 
             _updateAnalysisWorker = new BackgroundWorker();
             _updateAnalysisWorker.DoWork += _updateAnalysisWorker_DoWork;
             _updateAnalysisWorker.RunWorkerAsync(this);
         }
 
+        #endregion
+
+        #region Public API
+
+        public Point Position()
+        {
+            return new Point(this.Left, this.Top);
+        }
+
+        public Size Size()
+        {
+            return new Size(this.ActualWidth, this.ActualHeight);
+        }
+
+        #endregion
+
+        #region Window Overrides
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+        }
+
         protected override void OnInitialized(EventArgs e)
         {
             base.OnInitialized(e);
 
-            DisableAnalysisUpdate();
-
             _startUpdatingButton.Click += (sStart, eStart) => { EnableAnalysisUpdate(); };
             _stopUpdatingButton.Click += (sStop, eStop) => { DisableAnalysisUpdate(); };
         }
+
+        #endregion
+
+        #region Helper Methods
 
         private void DisableAnalysisUpdate()
         {
@@ -49,6 +87,8 @@ namespace wpfScope
                 _shouldUpdateAnalysis = true;
             }
         }
+
+        #endregion
 
         #region Update Background Worker Handlers
 
@@ -67,10 +107,17 @@ namespace wpfScope
                 {
                     var window = e.Argument as DimensionsOverlay;
 
-                    if (window != null)
+                    if (Analyzer != null && window != null)
                     {
                         // Actually update the analysis.
                         System.Diagnostics.Debug.WriteLine("Updating analysis...");
+
+                        Point windowPosition = window.Position();
+                        Size windowSize = window.Size();
+
+                        Analyzer.Screenshot = ScreenshotUtility.CaptureRegion(
+                            (int)windowPosition.X, (int)windowPosition.Y,
+                            (int)windowSize.Width, (int)windowSize.Height);
                     }
                 }
 
