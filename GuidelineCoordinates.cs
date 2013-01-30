db19384cecc7ff8e8ce47d995f78045abfbc69e8
Benry Yip <benry.yip@ni.com>
2013-01-30 14:39:09 -0600

    Moved coordinate classes to their own files.

---
+++ GuidelineCoordinates.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace wpfScope
+{
+    public class GuidelineCoordinates : INotifyPropertyChanged
+    {
+        #region Private Fields
+
+        private const int _capLength = 2;
+
+        #endregion
+
+        #region Properties
+
+        public static readonly string HorizontalGuidelinePropertyName = "HorizontalGuideline";
+        private LineCoordinates _horizontalGuideline;
+        public LineCoordinates HorizontalGuideline
+        {
+            get { return _horizontalGuideline; }
+            set
+            {
+                if (_horizontalGuideline != value)
+                {
+                    _horizontalGuideline = value;
+                    NotifyPropertyChanged(HorizontalGuidelinePropertyName);
+                }
+            }
+        }
+
+        public static readonly string VerticalGuidelinePropertyName = "VerticalGuideline";
+        private LineCoordinates _verticalGuideline;
+        public LineCoordinates VerticalGuideline
+        {
+            get { return _verticalGuideline; }
+            set
+            {
+                if (_verticalGuideline != value)
+                {
+                    _verticalGuideline = value;
+                    NotifyPropertyChanged(VerticalGuidelinePropertyName);
+                }
+            }
+        }
+
+        public LineCoordinates LeftCap { get { return new LineCoordinates(new Point(), new Point()); } }
+        public LineCoordinates RightCap { get { return new LineCoordinates(new Point(), new Point()); } }
+        public LineCoordinates TopCap { get { return new LineCoordinates(new Point(), new Point()); } }
+        public LineCoordinates BottomCap { get { return new LineCoordinates(new Point(), new Point()); } }
+
+        #endregion
+
+        #region Constructor
+
+        public GuidelineCoordinates()
+        {
+            HorizontalGuideline = new LineCoordinates();
+            VerticalGuideline = new LineCoordinates();
+        }
+
+        #endregion
+
+        #region API
+
+        public void UpdateHorizontalGuideline(double x1, double y1, double x2, double y2)
+        {
+            HorizontalGuideline.Update(x1, y1, x2, y2);
+            NotifyPropertyChanged(HorizontalGuidelinePropertyName);
+        }
+
+        public void UpdateVerticalGuideline(double x1, double y1, double x2, double y2)
+        {
+            VerticalGuideline.Update(x1, y1, x2, y2);
+            NotifyPropertyChanged(VerticalGuidelinePropertyName);
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ LineCoordinates.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+
+namespace wpfScope
+{
+    public class LineCoordinates
+    {
+        #region Properties
+
+        private Point _start;
+        public Point Start { get { return _start; } }
+
+        private Point _end;
+        public Point End { get { return _end; } }
+
+        #endregion
+
+        #region Constructors
+
+        public LineCoordinates() : this(new Point(), new Point()) { }
+
+        public LineCoordinates(Point start, Point end)
+        {
+            _start = start;
+            _end = end;
+        }
+
+        #endregion
+
+        #region API
+
+        public void Update(double x1, double y1, double x2, double y2)
+        {
+            _start.X = x1;
+            _start.Y = y1;
+            _end.X = x2;
+            _end.Y = y2;
+        }
+
+        #endregion
+
+        #region Comparison Overrides
+
+        public override bool Equals(object obj)
+        {
+            return obj is LineCoordinates && this == (LineCoordinates)obj;
+        }
+
+        public static bool operator ==(LineCoordinates lc1, LineCoordinates lc2)
+        {
+            if (object.ReferenceEquals(lc1, null) || object.ReferenceEquals(lc2, null)) { return false; }
+            return lc1.Start == lc2.Start && lc1.End == lc2.End;
+        }
+
+        public static bool operator !=(LineCoordinates lc1, LineCoordinates lc2)
+        {
+            return !(lc1 == lc2);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        #endregion
+    }
+}
